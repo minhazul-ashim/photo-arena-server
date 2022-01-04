ba56a1381e0ff5b6597b229d2f3fdeb9880394ac
Minhazul Ashim <ashim.ashim7470@gmail.com>
2022-01-04 19:32:50 +0600

    some apis developed

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const admin = require("firebase-admin");
 
 const serviceAccount = require("./pixel-art-254-firebase-adminsdk-po8rq-3f3288ca10.json");
 
